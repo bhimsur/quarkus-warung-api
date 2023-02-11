03ef9cffc4f271e6e5ff11dfef55d9a390abae60
bhimsur <bhimsur>
2023-02-11 09:50:52 +0700

    :sparkles: add constant - request mapper

---
+++ src/main/kotlin/io/bhimsur/app/web/model/constant/RequestMapper.kt
@@ -0,0 +1,7 @@
+package io.bhimsur.app.web.model.constant
+
+object RequestMapper {
+    const val LOGIN: String = "/login"
+    const val REGISTER: String = "/register"
+    const val USER: String = "/user"
+}
