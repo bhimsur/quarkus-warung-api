d5a2643b5e436ed214d4d9fae6b10d179c23a61f
bhimsur <bhimsur>
2023-02-14 11:22:32 +0700

    :lock: implement rsa decrypt at user login

--- src/main/kotlin/io/bhimsur/domain/feature/impl/UserLoginImpl.kt
+++ src/main/kotlin/io/bhimsur/domain/feature/impl/UserLoginImpl.kt
@@ -3,22 +3,26 @@ package io.bhimsur.domain.feature.impl
 import io.bhimsur.domain.exception.NotFoundException
 import io.bhimsur.domain.exception.WrongPasswordException
 import io.bhimsur.domain.feature.UserLogin
+import io.bhimsur.domain.model.provider.CryptoProvider
 import io.bhimsur.domain.model.provider.HashProvider
 import io.bhimsur.domain.model.user.User
 import io.bhimsur.domain.model.user.UserLoginInput
 import io.bhimsur.domain.model.user.UserRepository
 import java.util.*
+import javax.inject.Named
 
 class UserLoginImpl(
     private val hashProvider: HashProvider,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    @Named(value = "aesProvider") private val cryptoProvider: CryptoProvider
 ) : UserLogin {
     override fun command(request: UserLoginInput): User {
         val user: Optional<User> = userRepository.findByUsername(request.username)
+        val password: String = cryptoProvider.decrypt(request.password)
         if (user.isEmpty) {
             throw NotFoundException("Username Not Found")
         }
-        if (!isPasswordValid(request.password, user.get().password)) {
+        if (!isPasswordValid(password, user.get().password)) {
             throw WrongPasswordException()
         }
         return user.get()
