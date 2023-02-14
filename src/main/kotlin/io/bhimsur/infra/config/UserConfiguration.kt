1840f30c160cf9f9a1eac955827372f7ff0c3f17
bhimsur <bhimsur>
2023-02-14 11:22:03 +0700

    :bug: define bean in user configuration

--- src/main/kotlin/io/bhimsur/infra/config/UserConfiguration.kt
+++ src/main/kotlin/io/bhimsur/infra/config/UserConfiguration.kt
@@ -4,24 +4,40 @@ import io.bhimsur.domain.feature.UserLogin
 import io.bhimsur.domain.feature.UserRegistration
 import io.bhimsur.domain.feature.impl.UserLoginImpl
 import io.bhimsur.domain.feature.impl.UserRegistrationImpl
+import io.bhimsur.domain.model.provider.CryptoProvider
 import io.bhimsur.domain.model.provider.HashProvider
 import io.bhimsur.domain.model.user.UserModelBuilder
 import io.bhimsur.domain.model.user.UserRepository
+import io.bhimsur.domain.validator.ModelValidator
 import javax.enterprise.context.Dependent
+import javax.inject.Named
 import javax.inject.Singleton
+import javax.validation.Validator
 import javax.ws.rs.Produces
 
 @Dependent
 class UserConfiguration {
     @Produces
     @Singleton
-    fun userLogin(userRepository: UserRepository, hashProvider: HashProvider): UserLogin {
-        return UserLoginImpl(hashProvider, userRepository)
+    fun userLogin(userRepository: UserRepository, hashProvider: HashProvider, @Named("rsaProvider") cryptoProvider: CryptoProvider): UserLogin {
+        return UserLoginImpl(hashProvider, userRepository, cryptoProvider)
     }
 
     @Produces
     @Singleton
     fun userRegistration(userRepository: UserRepository, hashProvider: HashProvider, userModelBuilder: UserModelBuilder): UserRegistration {
         return UserRegistrationImpl(userRepository, hashProvider, userModelBuilder)
+    }
+
+    @Produces
+    @Singleton
+    fun userModelBuilder(modelValidator: ModelValidator): UserModelBuilder {
+        return UserModelBuilder(modelValidator)
+    }
+
+    @Produces
+    @Singleton
+    fun modelValidator(validator: Validator): ModelValidator {
+        return ModelValidator(validator)
     }
 }
