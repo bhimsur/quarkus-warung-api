2742b0f90c0c8d5c7621cb73c8376dce339a48c3
bhimsur <bhimsur>
2023-02-18 00:09:59 +0700

    :sparkles: enhance wallet repository

--- src/main/kotlin/io/bhimsur/domain/model/wallet/WalletRepository.kt
+++ src/main/kotlin/io/bhimsur/domain/model/wallet/WalletRepository.kt
@@ -4,7 +4,7 @@ import java.util.Optional
 import java.util.UUID
 
 interface WalletRepository {
-    fun topUp(wallet: Wallet)
-    fun withdraw(wallet: Wallet)
+    fun topUp(wallet: Wallet): Wallet
+    fun withdraw(wallet: Wallet): Wallet
     fun findByUserId(id: UUID): Optional<Wallet>
 }
